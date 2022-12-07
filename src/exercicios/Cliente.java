eac015653aa0d2123cac0064e66994b0fef7bc68
Renato Fernandes <renatodepsf@gmail.com>
2022-12-07 14:38:54 -0300

    Exercitando

---
+++ src/exercicios/Cliente.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class Cliente {
+
+    String nome, email;
+
+    // construtor
+    public Cliente(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+}
--- src/exercicios/Main.java
+++ src/exercicios/Main.java
@@ -1,5 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+
+        // inserir a quantidade de quartos
+        System.out.print("Quantos quartos você deseja alugar? ");
+        int n = sc.nextInt();
+        int[] quartos = new int[n];
+
+        // inserir dados do cliente
+
+
+
+
+
     }
 }
