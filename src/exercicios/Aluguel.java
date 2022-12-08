37125581253dc2a6b5eaf44d3994c73e97915eb0
Renato Fernandes <renatodepsf@gmail.com>
2022-12-08 15:04:00 -0300

    Exercitando

---
+++ src/exercicios/Aluguel.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+public class Aluguel {
+
+    String nome, email;
+
+    public Aluguel(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
--- src/exercicios/Main.java
+++ src/exercicios/Main.java
@@ -1,21 +1,28 @@
 package exercicios;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+        List<String> lista = new ArrayList<>();
+
+        // adicionar elementos na lista
+        lista.add("Renato");
+        lista.add("Tairine");
+        lista.add("Cauã");
+        lista.add("Abraão");
+
+        lista.remove(3);
+        System.out.println(lista.indexOf("Cauã"));
 
 
-        // inserir a quantidade de quartos
-        System.out.print("Quantos quartos você deseja alugar? ");
-        int n = sc.nextInt();
-        int[] quartos = new int[n];
-
-        // inserir dados do cliente
-
-
-
+        // for each para imprimir na tela
+        for (String x : lista) {
+            System.out.println(x);
+        }
 
 
     }
